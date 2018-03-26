073106611b630f17ca319730eb2e04f0930282d7
Dominik Kroliczek <dominik.kroliczek@pragmaticcoders.com>
2018-03-26 15:26:01 +0200

    Change external csv to hardcoded initial data

--- migrations/3_add_initial_hodlers.ts
+++ migrations/3_add_initial_hodlers.ts
@@ -1,4 +1,3 @@
-import * as fs from 'fs';
 import { HodlingsArtifacts } from 'hodlings';
 import { Deployer } from 'truffle';
 import { AnyNumber } from 'web3';
@@ -7,47 +6,25 @@ declare const artifacts: HodlingsArtifacts;
 
 const PragmaticHodlings = artifacts.require('./PragmaticHodlings.sol');
 
-async function deploy() {
-  const initialHodlers: InitialHodler[] = getInitialHodlers();
+const initialHodlers: InitialHodler[] = [
+  ['0x9838e38f8273ee06251d2ef7ac3d4312df3c150d', '2015-01-02'],
+  ['0x6b734e6151d6ca0b03eab35d48911f66fe6b3a90', '2016-01-02'],
+  ['0xcba5a25918f46ea73f2640cecb8e0dda6cfec4ef', '2017-01-02'],
+  ['0x4cbd4b81d620e71aded75e8a05f6c90e2f9aaa97', '2018-01-02']
+].map(arr => ({address: arr[0], joinDate: toTimestamp(arr[1])}));
 
+async function deploy() {
   const hodlings = await PragmaticHodlings.deployed();
 
   for (const hodler of initialHodlers) {
     // tslint:disable:no-console
     console.log('adding hodler', hodler);
-    await hodlings.addHodler(hodler.address, hodler.joined);
+    await hodlings.addHodler(hodler.address, hodler.joinDate);
   }
-
 }
 
-interface InitialHodler {
-  address: Address;
-  joined: AnyNumber;
-}
-
-function getInitialHodlers(): InitialHodler[] {
-  const result: InitialHodler[] = [];
-  const lines = fs.readFileSync('./migrations/initialHodlings.csv', 'utf-8')
-    .split('\n');
-
-  lines.forEach((line, idx) => {
-    if (idx === 0) {
-      // address,date(YYYY-MM-DD)
-      return;
-    }
-    const splitted = line.split(',');
-    result.push({
-      address: splitted[0],
-      joined: toTimestamp(splitted[1])
-    });
-
-  });
-
-  return result;
-
-  function toTimestamp(date: string) {
-    return Math.floor(Date.parse(date) / 1000);
-  }
+function toTimestamp(date: string) {
+  return Math.floor(Date.parse(date) / 1000);
 }
 
 function migrate(deployer: Deployer) {
@@ -55,3 +32,8 @@ function migrate(deployer: Deployer) {
 }
 
 export = migrate;
+
+interface InitialHodler {
+  address: Address;
+  joinDate: AnyNumber;
+}
